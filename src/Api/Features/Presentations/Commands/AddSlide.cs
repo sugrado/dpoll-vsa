1b96701396b9cc0bafdfb2617bd5e1a034dd96f4
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2024-08-31 19:11:22 +0300

    feat: create add slide to presentation command and unit tests

---
+++ src/Api/Features/Presentations/Commands/AddSlide.cs
@@ -0,0 +1,87 @@
+﻿using Api.Features.Slides.Domain.Entities;
+using Api.Features.Slides.Domain.Enums;
+using Api.Features.Slides.Infrastructure.Persistence;
+using Api.Shared;
+using Api.Shared.Behaviors.Transaction;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Features.Presentations.Commands;
+
+public partial class PresentationsController : ApiControllerBase
+{
+    [HttpPost("/api/presentations/{presentationId}/slides")]
+    public async Task<IActionResult> AddSlide([FromRoute] string presentationId, [FromBody] AddSlideDto addSlideDto)
+    {
+        AddSlideCommand command = new() { PresentationId = presentationId, Slide = addSlideDto };
+        await Mediator.Send(command);
+        return NoContent();
+    }
+}
+
+public record AddSlideDto(SlideType Type, short Order, AddSlideMultipleChoiceDto? MultipleChoice);
+public record AddSlideMultipleChoiceDto(string Title, string[] Choices);
+
+public class AddSlideCommandValidator : AbstractValidator<AddSlideCommand>
+{
+    public AddSlideCommandValidator()
+    {
+        RuleFor(v => v.Slide.Type).NotNull().NotEmpty().IsInEnum();
+        RuleFor(v => v.Slide.Order).NotNull().NotEmpty();
+        When(p => p.Slide.Type == SlideType.MultipleChoice, () =>
+        {
+            RuleFor(v => v.Slide.MultipleChoice).NotNull().NotEmpty();
+            RuleFor(v => v.Slide.MultipleChoice!.Title).NotNull().NotEmpty().MinimumLength(3).MaximumLength(256);
+            RuleFor(v => v.Slide.MultipleChoice!.Choices).Must(p => p != null && p.Length != 0);
+        });
+    }
+}
+
+public class AddSlideCommand : IRequest, ITransactionalRequest
+{
+    public required string PresentationId { get; set; }
+    public required AddSlideDto Slide { get; set; }
+
+    public class AddSlideCommandHandler(ISlideRepository slideRepository, IMultipleChoiceRepository multipleChoiceRepository) : IRequestHandler<AddSlideCommand>
+    {
+        public async Task Handle(AddSlideCommand request, CancellationToken cancellationToken)
+        {
+            Slide slide = new()
+            {
+                PresentationId = Guid.Parse(request.PresentationId),
+                Type = request.Slide.Type,
+                Order = request.Slide.Order
+            };
+
+            await slideRepository.AddAsync(slide);
+
+            switch (request.Slide.Type)
+            {
+                case SlideType.MultipleChoice:
+                    await CreateMultipleChoiceSlide(slide, request.Slide.MultipleChoice!, cancellationToken);
+                    break;
+                case SlideType.Video:
+                    //Video creation logic
+                    break;
+            }
+        }
+
+        private async Task CreateMultipleChoiceSlide(Slide slide, AddSlideMultipleChoiceDto multipleChoiceDto, CancellationToken cancellationToken)
+        {
+            List<MultipleChoiceOption> options = multipleChoiceDto
+                .Choices
+                .Select(p => new MultipleChoiceOption { Value = p })
+                .ToList();
+
+            MultipleChoice multipleChoice = new()
+            {
+                Slide = slide,
+                Options = options,
+                Title = multipleChoiceDto.Title
+            };
+
+            await multipleChoiceRepository.AddAsync(multipleChoice);
+        }
+    }
+}
---
+++ tests/Features.Tests/Features/Presentations/Commands/AddSlideCommandHandlerTests.cs
@@ -0,0 +1,73 @@
+﻿using Api.Features.Presentations.Commands;
+using Api.Features.Slides.Domain.Entities;
+using Api.Features.Slides.Domain.Enums;
+using Api.Features.Slides.Infrastructure.Persistence;
+using FluentValidation.Results;
+using Moq;
+using static Api.Features.Presentations.Commands.AddSlideCommand;
+
+namespace Features.Tests.Features.Presentations.Commands;
+
+public class AddSlideCommandHandlerTests
+{
+    private readonly Mock<ISlideRepository> _slideRepositoryMock;
+    private readonly Mock<IMultipleChoiceRepository> _multipleChoiceRepositoryMock;
+    private readonly AddSlideCommandHandler _handler;
+    private readonly AddSlideCommandValidator _addSlideCommandValidator;
+
+    public AddSlideCommandHandlerTests()
+    {
+        _slideRepositoryMock = new Mock<ISlideRepository>();
+        _multipleChoiceRepositoryMock = new Mock<IMultipleChoiceRepository>();
+        _addSlideCommandValidator = new AddSlideCommandValidator();
+        _handler = new AddSlideCommandHandler(_slideRepositoryMock.Object, _multipleChoiceRepositoryMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldCreateMultipleChoiceSlide_WhenMultipleChoiceSlideIsGiven()
+    {
+        // Arrange
+        var command = new AddSlideCommand
+        {
+            PresentationId = Guid.NewGuid().ToString(),
+            Slide = new AddSlideDto(
+                SlideType.MultipleChoice,
+                1,
+                new AddSlideMultipleChoiceDto("Test Title", ["Choice1", "Choice2"])
+            )
+        };
+
+        // Act
+        await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        _slideRepositoryMock.Verify(r => r.AddAsync(It.IsAny<Slide>()), Times.Once);
+        _multipleChoiceRepositoryMock.Verify(r => r.AddAsync(It.Is<MultipleChoice>(mc =>
+            mc.Title == command.Slide.MultipleChoice!.Title &&
+            mc.Options!.Count == 2 &&
+            mc.Options.All(o => o.Value == "Choice1" || o.Value == "Choice2")
+        )), Times.Once);
+    }
+
+    [Fact]
+    public void Handle_ShouldThrowException_WhenInvalidTitle()
+    {
+        // Arrange
+        var presentationId = Guid.NewGuid().ToString();
+        var addSlideDto = new AddSlideDto(SlideType.MultipleChoice, 1, new AddSlideMultipleChoiceDto("a", ["Choice1", "Choice2"]));
+        var command = new AddSlideCommand
+        {
+            PresentationId = presentationId,
+            Slide = addSlideDto
+        };
+
+        // Act
+        ValidationFailure? result = _addSlideCommandValidator
+            .Validate(command)
+            .Errors
+            .FirstOrDefault(x => x.PropertyName == "Slide.MultipleChoice.Title" && x.ErrorCode == "MinimumLengthValidator");
+
+        // Assert
+        Assert.Equal("MinimumLengthValidator", result?.ErrorCode);
+    }
+}
