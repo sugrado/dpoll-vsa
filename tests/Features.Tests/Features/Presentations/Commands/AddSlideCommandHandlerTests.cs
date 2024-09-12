cc05da1d75a5be3883274fccd01a4988fcb584bc
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2024-09-12 13:21:50 +0300

    test: add slide command handler tests

--- tests/Features.Tests/Features/Presentations/Commands/AddSlideCommandHandlerTests.cs
+++ tests/Features.Tests/Features/Presentations/Commands/AddSlideCommandHandlerTests.cs
@@ -1,52 +1,89 @@
-﻿using Api.Features.Presentations.Commands;
+﻿using Api.Features.Presentations;
+using Api.Features.Presentations.Commands;
+using Api.Features.Presentations.Domain.Entities;
+using Api.Features.Slides;
 using Api.Features.Slides.Domain.Entities;
 using Api.Features.Slides.Domain.Enums;
-using Api.Features.Slides.Infrastructure.Persistence;
+using Api.Shared.Exceptions;
 using FluentValidation.Results;
-using Moq;
+using NSubstitute;
+using System.Linq.Expressions;
+using System.Text.Json;
 using static Api.Features.Presentations.Commands.AddSlideCommand;
 
 namespace Features.Tests.Features.Presentations.Commands;
 
 public class AddSlideCommandHandlerTests
 {
-    private readonly Mock<ISlideRepository> _slideRepositoryMock;
-    private readonly Mock<IMultipleChoiceRepository> _multipleChoiceRepositoryMock;
+    private readonly ISlideRepository _slideRepository;
+    private readonly IPresentationRepository _presentationRepository;
     private readonly AddSlideCommandHandler _handler;
     private readonly AddSlideCommandValidator _addSlideCommandValidator;
 
     public AddSlideCommandHandlerTests()
     {
-        _slideRepositoryMock = new Mock<ISlideRepository>();
-        _multipleChoiceRepositoryMock = new Mock<IMultipleChoiceRepository>();
+        _slideRepository = Substitute.For<ISlideRepository>();
+        _presentationRepository = Substitute.For<IPresentationRepository>();
         _addSlideCommandValidator = new AddSlideCommandValidator();
-        _handler = new AddSlideCommandHandler(_slideRepositoryMock.Object, _multipleChoiceRepositoryMock.Object);
+
+        _handler = new AddSlideCommandHandler(_slideRepository, _presentationRepository);
     }
 
     [Fact]
-    public async Task Handle_ShouldCreateMultipleChoiceSlide_WhenMultipleChoiceSlideIsGiven()
+    public async Task Handle_Should_Add_Slide_When_Presentation_Exists()
     {
         // Arrange
+        var presentationId = Guid.NewGuid().ToString();
+        var addSlideDto = new AddSlideDto(SlideType.MultipleChoice, 1, JsonDocument.Parse("{\"question\": \"What is your favorite color?\"}"));
+
         var command = new AddSlideCommand
         {
-            PresentationId = Guid.NewGuid().ToString(),
-            Slide = new AddSlideDto(
-                SlideType.MultipleChoice,
-                1,
-                new AddSlideMultipleChoiceDto("Test Title", ["Choice1", "Choice2"])
-            )
+            PresentationId = presentationId,
+            Slide = addSlideDto
         };
+
+        var presentation = new Presentation { Id = Guid.Parse(presentationId), Name = "My First Presentation" };
+
+        _presentationRepository.GetAsync(
+            Arg.Any<Expression<Func<Presentation, bool>>>(),
+            null, false, true, Arg.Any<CancellationToken>()).Returns(presentation);
 
         // Act
         await _handler.Handle(command, CancellationToken.None);
 
         // Assert
-        _slideRepositoryMock.Verify(r => r.AddAsync(It.IsAny<Slide>()), Times.Once);
-        _multipleChoiceRepositoryMock.Verify(r => r.AddAsync(It.Is<MultipleChoice>(mc =>
-            mc.Title == command.Slide.MultipleChoice!.Title &&
-            mc.Options!.Count == 2 &&
-            mc.Options.All(o => o.Value == "Choice1" || o.Value == "Choice2")
-        )), Times.Once);
+        await _slideRepository.Received(1).AddAsync(Arg.Is<Slide>(s =>
+            s.PresentationId == presentation.Id &&
+            s.Type == SlideType.MultipleChoice &&
+            s.Order == 1 &&
+            s.Visible &&
+            s.Content == addSlideDto.Content
+        ));
+    }
+
+    [Fact]
+    public async Task Handle_Should_Throw_NotFoundException_When_Presentation_Does_Not_Exist()
+    {
+        // Arrange
+        var presentationId = Guid.NewGuid().ToString();
+        var addSlideDto = new AddSlideDto(SlideType.MultipleChoice, 1, JsonDocument.Parse("{\"question\": \"What is your favorite color?\"}"));
+
+        var command = new AddSlideCommand
+        {
+            PresentationId = presentationId,
+            Slide = addSlideDto
+        };
+
+        _presentationRepository.GetAsync(
+            Arg.Any<Expression<Func<Presentation, bool>>>(),
+            null, false, true, Arg.Any<CancellationToken>()).Returns((Presentation?)null);
+
+        // Act & Assert
+        var exception = await Assert.ThrowsAsync<NotFoundException>(() => _handler.Handle(command, CancellationToken.None));
+        Assert.Equal("Presentation not found.", exception.Message);
+
+        // Ensure slide is not added
+        await _slideRepository.DidNotReceive().AddAsync(Arg.Any<Slide>());
     }
 
     [Fact]
@@ -54,7 +91,8 @@ public class AddSlideCommandHandlerTests
     {
         // Arrange
         var presentationId = Guid.NewGuid().ToString();
-        var addSlideDto = new AddSlideDto(SlideType.MultipleChoice, 1, new AddSlideMultipleChoiceDto("a", ["Choice1", "Choice2"]));
+        var addSlideDto = new AddSlideDto(default, 1, JsonDocument.Parse("{\"question\": \"What is your favorite color?\"}"));
+
         var command = new AddSlideCommand
         {
             PresentationId = presentationId,
@@ -65,9 +103,9 @@ public class AddSlideCommandHandlerTests
         ValidationFailure? result = _addSlideCommandValidator
             .Validate(command)
             .Errors
-            .FirstOrDefault(x => x.PropertyName == "Slide.MultipleChoice.Title" && x.ErrorCode == "MinimumLengthValidator");
+            .FirstOrDefault(x => x.PropertyName == "Slide.Type" && x.ErrorCode == "NotEmptyValidator");
 
         // Assert
-        Assert.Equal("MinimumLengthValidator", result?.ErrorCode);
+        Assert.Equal("NotEmptyValidator", result?.ErrorCode);
     }
 }
